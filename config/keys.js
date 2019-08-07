856049ed59578080fe843eb70f40cf8bcf33839b
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-07 00:09:37 -0400

    created config folder and keys.js to store MongoDBURI

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb+srv://codeOfTheFuture:H$e00ss1@learn-the-mern-stack-9tcmd.mongodb.net/test?retryWrites=true&w=majority',
+};
--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('bodyParser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
