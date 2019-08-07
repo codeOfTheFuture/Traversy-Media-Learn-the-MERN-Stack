6c651aa0a8d71b8f38fd4330c08e86c178d946a2
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-07 00:14:02 -0400

    server can run on process.env.Port or 5000

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
