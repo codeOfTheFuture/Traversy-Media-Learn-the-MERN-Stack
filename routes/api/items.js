8e0e105cf0ff73b70d3a3428347efd921b574d31
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-17 20:29:27 -0400

    create endpoint to /api/items/:id DELETE request to db to remove and item by id - back end complete

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // @route POST api/items
-// @desc Create A Item
+// @desc Create An Item
 // @access Public
 router.post('/', (req, res) => {
   const newItem = new Item({
@@ -22,6 +22,15 @@ router.post('/', (req, res) => {
   });
 
   newItem.save().then(item => res.json(item));
+});
+
+// @route DELETE api/items/:id
+// @desc Delete An Item
+// @access Public
+router.delete('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({ success: true })))
+    .catch(err => res.status(404).json({ success: false }));
 });
 
 module.exports = router;
